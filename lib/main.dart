4027b4640ba1da2abd43be08caccc3035bc6d060
tacowasabii <godseoril812@gmail.com>
2023-02-08 01:00:14 +0900

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -132,6 +132,101 @@ class HomePage extends StatelessWidget {
           ))
         ],
       ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              margin: const EdgeInsets.all(0),
+              // padding: const EdgeInsets.only(bottom:
+              decoration: const BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.network(
+                          "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                          width: 62,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const Text(
+                      "닉네임",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      "hello@world.com",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            AspectRatio(
+              aspectRatio: 12 / 4,
+              child: PageView(
+                children: [
+                  Image.network(
+                    "https://i.ibb.co/Q97cmkg/sale-event-banner1.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/GV78j68/sale-event-banner2.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/R3P3RHw/sale-event-banner3.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/LRb1VYs/sale-event-banner4.jpg",
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                '구매내역',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text(
+                '저장한 레시피',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
